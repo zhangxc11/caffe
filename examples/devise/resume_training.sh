afbea82a53689dcd2d2da951bf5aa181d2665982
zhangxc11 <xingchengzhang@sina.com>
2014-08-01 17:45:27 -0700

    edit proto file to train 	修改：      ../devise/devise_solver.prototxt 	修改：      ../devise/devise_train.prototxt 	修改：      ../devise/resume_training.sh 	修改：      ../imagenet/imagenet_solver.prototxt 	修改：      ../imagenet/imagenet_train.prototxt

--- examples/devise/resume_training.sh
+++ examples/devise/resume_training.sh
@@ -3,6 +3,6 @@
 TOOLS=../../build/tools
 
 GLOG_logtostderr=1 $TOOLS/train_net.bin \
-    imagenet_solver.prototxt caffe_imagenet_train_iter_60000.solverstate
+    devise_solver.prototxt a.solverstate
 
 echo "Done."
